fcd56d6323a34d7762c6f9b462216e8ff6623dad
chhatraraj <chhatraneupane999@gmail.com>
2026-06-04 12:39:22 +0545

    chore: scaffold project folders and docker stack
    - Set up React/Vite/Tailwind v4 client layout
    - Scaffold FastAPI async routers, layers, and repos
    - Configure Docker Compose for client, backend, and postgres

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline text-center bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text"> 
+    Hello Team !
+  </h1>
+  <p className="mt-2 text-slate-700 text-center">
+       
+       lets is ready to roll.
+      </p>
+    </>
+  )
+}
+
+export default App
