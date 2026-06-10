3d708ef2daf4d216c7e893ad8937488e39c19913
chhatraraj <chhatraneupane999@gmail.com>
2026-06-10 10:38:45 +0545

    feat(integration): connect React frontend with FastAPI backend

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -4,4 +4,4 @@ router = APIRouter()
 
 @router.post("/login")
 async def login():
-    return {"access_token": "routes_check_gareko", "token_type": "bearer"}
+    return {"access_token": "routes work", "token_type": "bearer"}
